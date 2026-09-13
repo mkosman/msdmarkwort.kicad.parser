3d8060278021b234936afc5b6a8c2fbefd55ee4d
Marek Kosman <marek.kosman@fmcode.pl>
2026-09-13 13:44:18 +0200

    Update Segment class to use KicadParserComplexSymbol for StartPosition and EndPosition

--- src/MSDMarkwort.Kicad.Parser.PcbNew/Models/PartSegment/Segment.cs
+++ src/MSDMarkwort.Kicad.Parser.PcbNew/Models/PartSegment/Segment.cs
@@ -6,10 +6,10 @@ namespace MSDMarkwort.Kicad.Parser.PcbNew.Models.PartSegment
 {
     public class Segment
     {
-        [KicadParserSymbol("start")]
+        [KicadParserComplexSymbol("start")]
         public Position StartPosition { get; set; } = new Position();
 
-        [KicadParserSymbol("end")]
+        [KicadParserComplexSymbol("end")]
         public Position EndPosition { get; set; } = new Position();
 
         [KicadParserSymbol("width")]
@@ -26,7 +26,7 @@ namespace MSDMarkwort.Kicad.Parser.PcbNew.Models.PartSegment
 
         [KicadParserSymbol("uuid")]
         public Guid Uuid { get; set; }
-
+         
         [KicadParserSymbol("tstamp")]
         public Guid TStamp { get; set; }
     }
